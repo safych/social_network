92b157912699f99668b63f47b20dbb4e94037da7
safych <v7lad@ukr.net>
2024-06-26 13:14:01 +0300

    Added new locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  root "home#index"
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root "home#index"
 
-  devise_for :admins
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+    devise_for :admins
+    devise_for :users, controllers: {
+      registrations: 'users/registrations',
+      sessions: 'users/sessions',
+      passwords: 'users/passwords'
+    }
+  end
 end
