d556d6cb4293e84c0ea20f294acbc186078f7db9
safych <v7lad@ukr.net>
2024-06-27 13:45:24 +0300

    Updated user's registrations_controller, model and view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :name, :surname, :born_on, :username, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :name, :surname, :born_on, :username, :gender, :avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :name, :surname, :born_on, :username, :gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :name, :surname, :born_on, :username, :gender, :avatar])
   end
 
   # The path used after sign up.
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,26 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
 
-  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/ }
-  validates :phone, presence: true, uniqueness: true, format: { with: /\A\+?[1-9]\d{1,14}\z/ }
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: :invalid }
+  validates :phone, presence: true, uniqueness: true, format: { with: /\A\+?[1-9]\d{1,14}\z/, message: :invalid }
   validates :encrypted_password, presence: true
-  validates :name, presence: true, format: { with: /\A.{1,30}\z/ }
-  validates :surname, presence: true, format: { with: /\A.{1,30}\z/ }
+  validates :name, presence: true, format: { with: /\A.{1,30}\z/, message: :invalid }
+  validates :surname, presence: true, format: { with: /\A.{1,30}\z/, message: :invalid }
   validates :born_on, presence: true
-  validates :username, presence: true, format: { with: /\A.{1,25}\z/ }
-  validates :gender, presence: true, inclusion: { in: %w(man woman), 
-                                                  message: "%{value} is not a valid gender" }
+  validate :born_on_validation
+  validates :username, presence: true, format: { with: /\A.{1,25}\z/, message: :invalid }
+  validates :gender, presence: true, inclusion: { in: %w(man woman) }
+
+  private
+
+  def born_on_validation
+    if born_on.present? && born_on > Date.today
+      errors.add(:born_on, I18n.t("cannot_be_in_the_future"))
+    end
+  end
 end
