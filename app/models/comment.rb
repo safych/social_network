21e68bdf675527970aff4132c4e12c837e4d823e
safych <v7lad@ukr.net>
2024-07-01 13:21:47 +0300

    Destroyed likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  has_many :likes, as: :likeable, dependent: :destroy
+  # has_many :likes, as: :likeable, dependent: :destroy
 
   validates :description, presence: true, length: { maximum: 150 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_many :comments, dependent: :destroy
-  has_many :likes, as: :likeable, dependent: :destroy
+  # has_many :likes, as: :likeable, dependent: :destroy
 
   has_one_attached :avatar
 
